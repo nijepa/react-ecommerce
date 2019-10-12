9fcedeb308af41e7238d4871039c47543cc4cf3a
Nikola Pavicevic <nijepa@gmail.com>
2019-10-12 15:31:15 +0200

    Created directory and shop reducers & collection-overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data'
+import CollectionsOverview from '../../components/collections-owerview/collections-overview.component'
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    };
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections
-                        .filter((item, idx) => idx < 4)
-                        .map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}  />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const ShopPage = () => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+);
 
 export default ShopPage;
