60d891902ee573dcc96fcb025a359766cbcc8c4c
Nikola Pavicevic <nijepa@gmail.com>
2019-10-13 02:06:12 +0200

    Implemented styled-components & moved local data to Firebase, created shop-action to get data from Firestore

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -12,33 +11,33 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss';
+import { HeaderContainer, OptionsContainer, LogoContainer, OptionLink, UserNameContainer } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {currentUser ? (
-                <div className='option' onClick={() => auth.signOut()}>
-                    <span className='user-name'>&#10038; {currentUser.displayName} &#10038;</span> SIGN OUT
-                </div>
+                <OptionLink as='div' onClick={() => auth.signOut()}>
+                    <UserNameContainer>&#10038; {currentUser.displayName} &#10038;</UserNameContainer> SIGN OUT
+                </OptionLink>
             ) : (
-                <Link className='option' to='/signin'>
+                <OptionLink to='/signin'>
                     SIGN IN
-                </Link>
+                </OptionLink>
             )}
             <CartIcon />
-        </div>
+        </OptionsContainer>
         { hidden ? null : <CartDropdown /> }
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Directory from'../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import { HomPageContainer } from './homepage.styles';
 
 const HomePage = () => (
-    <div className='homepage'>
+    <HomPageContainer>
         <Directory />
-    </div>
+    </HomPageContainer>
 );
 
 export default HomePage;
